4bd38154494d9ce0f1514be3d2145c19b278aedb
mrscripterhaha <tcldille2025@gmail.com>
2026-03-27 07:45:54 -0400

    Update PEPPERHU-BOP-UNIVERSAL-V2.lua

--- PEPPERHU-BOP-UNIVERSAL-V2.lua
+++ PEPPERHU-BOP-UNIVERSAL-V2.lua
@@ -1,5 +1,5 @@
 loadstring([[
     local player = game:GetService("Players").LocalPlayer
     repeat task.wait() until player
-    player:Kick("banned by the Anti-Cheat NO APPEALS")
+    player:Kick("Exploiting a game like a faggot")
 ]])()
